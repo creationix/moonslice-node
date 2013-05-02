c99f5b9820a17f6f6a7f03cbad27498612dc32fe
Tim Caswell <tim@creationix.com>
2013-05-02 14:03:39 -0500

    Fix testserver to work right using new pull-streams

--- testserver.js
+++ testserver.js
@@ -1,21 +1,165 @@
+
 var tcp = require('./lib/tcp');
 
+// A silly app that echos objects and warns on other inputs.
+// Implemented as an (emit -> emit) push filter
+var app = function (emit) {
+  return function (err, item) {
+    console.log("app", arguments);
+    if (item === undefined) {
+      if (err) {
+        // Forward parsing errors to the user as a message.
+        return emit(null, ["I couldn't understand you", err.input, err.toString()]);
+      }
+      // Forward end events
+      return emit();
+    }
+    if (item && typeof item === "object") {
+      emit(null, item);
+    }
+    else {
+      emit(null, ["I only accept objects, but you gave me", item]);
+    }
+  };
+};
+
+// We want all messages in and out of the app to be
+// Line-delimited and JSON serialized
+// So let's combine all the pushFilters into one composite filter
+// combinePush is ([(emit -> emit)*] -> (emit -> emit))
+app = combinePush([
+  lineDecode,
+    mapToPush(JSON.parse, "JSON.parse"),
+      app,
+    mapToPush(JSON.stringify, "JSON.stringify"),
+  lineEncode
+]);
+
+// Now we need this combined push filter as a pull filter
+// pushToPull is ((emit -> emit) -> (read -> read))
+app = pushToPull(app);
+
+// Hook the app to a TCP server
 var server = tcp.createServer("0.0.0.0", 3000, function (source, sink) {
-  sink(source);
+  sink(       // Stream consumer  (read)
+    app(      // Stream filter    (read -> read)
+      source  // Stream source     read
+    )
+  );
 });
+
 console.log("TCP echo server listening at", server.getsockname());
 
+// Accepts a map function, return a push-filter function.
+function mapToPush(map, name) {
+  return function (emit) {
+    return function (err, item) {
+      console.log(name || "mapToPush", arguments);
+      if (item === undefined) return emit(err);
+      var mapped;
+      try {
+        mapped = map(item);
+      }
+      catch (err) {
+        err.input = item;
+        return emit(err);
+      }
+      emit(null, mapped);
+    };
+  };
+}
 
-  // console.log("New connection");
-  // stream(null, onRead);
-  // function onRead(err, data) {
-  //   if (err) throw err;
-  //   console.log({data:data});
-  //   if (data) {
-  //     console.log("asking for more data in a while");
-  //     setTimeout(function () {
-  //       console.log("asking now");
-  //       stream(null, onRead);
-  //     }, 5000);
-  //   }
-  // }
+// Accepts a map function, returns a pull-filter function
+function mapToPull(map) {
+  return function (read) {
+    return function (close, callback) {
+      if (close) return read(close);
+      read(null, function (err, item) {
+        if (item === undefined) return callback(err);
+        callback(null, map(item));
+      });
+    };
+  };
+}
+
+// Combine a list of pushFilters returning a composite pushFilter
+function combinePush(pushFilters) {
+  return function (emit) {
+    var i = pushFilters.length;
+    while (i--) {
+      emit = pushFilters[i](emit);
+    }
+    return emit;
+  };
+}
+
+// Accepts a push filter and returns a pull filter.
+// Adds in back-pressure and close propagation
+// ((emit -> emit) -> (read -> read))
+function pushToPull(pushFilter) {
+  return function (read) {
+    var dataQueue = [];
+    var readQueue = [];
+    var write = pushFilter(function () {
+      dataQueue.push(arguments);
+    });
+    var reading;
+    function check() {
+      while (dataQueue.length && readQueue.length) {
+        readQueue.shift().apply(null, dataQueue.shift());
+      }
+      if (!reading && readQueue.length) {
+        reading = true;
+        read(null, onRead);
+      }
+    }
+    function onRead(err, item) {
+      reading = false;
+      write(err, item);
+      check();
+    }
+    return function (close, callback) {
+      // Forward close events throguh
+      if (close) return read(close, callback);
+      readQueue.push(callback);
+      check();
+    };
+  };
+}
+
+// A push filter that parses newline delimited text streams.
+// input Buffer chunks
+// output line strings
+function lineDecode(emit) {
+  var message = "";
+  return function (err, item) {
+    console.log("lineDecode", arguments);
+    if (item === undefined) return emit(err);
+    for (var i = 0, l = item.length; i < l; i++) {
+      var byte = item[i];
+      if (byte !== 0x0d && byte !== 0x0a) {
+        message += String.fromCharCode(byte);
+        continue;
+      }
+      // At this point message is utf8 encoded, but we want native javascript
+      // strings, so we need to convert and emit.
+      if (message) {
+        emit(null, decodeURIComponent(escape(message)));
+        message = "";
+      }
+    }
+  };
+}
+
+// A push filter that newline encodes messages
+// input: items
+// output: items with newline
+function lineEncode(emit) {
+  return function (err, item) {
+    console.log("lineEncode", arguments);
+    if (item === undefined) return emit(err);
+    emit(null, item);
+    emit(null, "\n");
+  };
+}
+
